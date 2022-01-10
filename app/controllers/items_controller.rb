d0ae9d1f060f10cfd9bb8ac229c0d3af82bc4d9a
shinopai <harbor.shinozaki@gmail.com>
2022-01-10 10:13:54 +0000

    alter review function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,13 +9,20 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @review = Review.new
+    @review_scores = Review.where(item_id: @item).pluck(:score)
+
+    if @review_scores.size == 1
+      @score_avg = @review_scores.sum.fdiv(@review_scores.length).to_i
+    else
+      @score_avg = @review_scores.sum.fdiv(@review_scores.length)
+    end
 
     if user_signed_in?
       @user = current_user
       @is_liked = Like.where(user_id: @user).where(item_id: @item).any?
       @is_reviewed_item = Review.where(user_id: @user).where(item_id: @item)
       logger.debug('start')
-      logger.debug(@is_reviewed_item[0].user.id)
+      logger.debug(@score_avg)
       logger.debug('end')
     end
   end
--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -2,4 +2,15 @@ module ReviewHelper
   def show_user_score(n)
       content_tag(:i, nil, class: "fas fa-star text-yellow-300") * n
   end
+
+  def show_score_avg(n)
+    if n.kind_of?(Integer)
+      content_tag(:i, nil, class: "fas fa-star text-yellow-300") * n +
+      content_tag(:i, nil, class: "far fa-star text-yellow-300") * (5 - n) if 5 - n != 0
+    else
+      content_tag(:i, nil, class: "fas fa-star text-yellow-300") * n.to_i +
+      content_tag(:i, nil, class: "fas fa-star-half-alt text-yellow-300") +
+      content_tag(:i, nil, class: "far fa-star text-yellow-300") * (5 - n.ceil) if 5 - n.ceil != 0
+    end
+  end
 end
